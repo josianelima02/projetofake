bc2e050d7b282170073cd09ec15b7ac12eb74098
josianelima02 <josiane.xavier02@hotmail.com>
2021-12-16 11:09:11 -0300

    site finalizado

---
+++ js/formu.js
@@ -0,0 +1,39 @@
+
+    //formulario
+    document.querySelector("#botaoEnviar").addEventListener("click", validarLogin)
+    function validarLogin() {
+
+        if (
+            document.getElementById("exampleInputPassword1").value != "" &&
+            document.getElementById("exampleInputEmail1").value != ""
+        ) {
+            const senha = document.getElementById("exampleInputPassword1")
+            const email = document.getElementById("exampleInputEmail1")
+
+            alert("Bem-vinda ao BOOKSTAN")
+            return false;
+        } else {
+
+            alert("Por favor, preencha os campos!")
+            return true;
+        }
+    };
+
+
+        //formulario
+        document.querySelector("#botaoCadastrar").addEventListener("click", validarFormulario)
+        function validarFormulario() {
+    
+            if (
+                document.getElementById("nome").value != "" &&
+                document.getElementById("email").value != ""
+            ) {
+                const nome = document.getElementById("nome")
+                email = document.getElementById("email")
+    
+                alert("Prontinho!! Você receberá um e-mail com os próximos passos!")
+            } else {
+    
+                alert("Por favor, preencha os campos!")
+            }
+        };
---
+++ js/main.js
@@ -0,0 +1,41 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+// Carroseul dos livros 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 28,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+/// Usuário some quando der 900px
+
+const usuario = document.getElementById('login.usuario');
+
